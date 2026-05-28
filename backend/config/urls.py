bd71b90102bbd447826d5b75c829ed8589bfeb30
Rodrigo Silvestre <rodrigo291002@hotmail.com>
2026-05-28 18:48:33 -0300

    Adiciona build do React para PythonAnywhere

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/posts/", include("posts.urls")),
     path("api/interactions/", include("interactions.urls")),
+    path("", TemplateView.as_view(template_name="index.html"), name="frontend"),
+    path("<path:path>", TemplateView.as_view(template_name="index.html"), name="frontend-routes"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
